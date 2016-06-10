b1d2b4fb01414bd6558f6df6d8898cf72b769694
LindaVels <linda.vels@idgis.nl>
2016-06-10 13:18:15 +0200

    serviceslist toegevoegd

--- both/router.js
+++ both/router.js
@@ -1,7 +1,7 @@
 Router.configure({
     layoutTemplate: 'main'
 });
-Router.route('/', 'services');
+Router.route('/', 'serviceslist');
 Router.route('/services/:_id', function () {
     var service = Services.findOne({_id: this.params._id});
     this.render('service', {data: service});
