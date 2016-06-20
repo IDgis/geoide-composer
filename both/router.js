03f9a8709308e9b424f5837bfd67c91644c7caa8
LindaVels <linda.vels@idgis.nl>
2016-06-20 10:02:20 +0200

    services versimpeld/opgeschoond

--- both/router.js
+++ both/router.js
@@ -2,23 +2,37 @@ Router.configure({
     layoutTemplate: 'main'
 });
 Router.route('/i18n', 'international');
-Router.route('/', 'serviceslist');
-Router.route('/services/:_id', function () {
+Router.route('/services', 'services');
+
+Router.route('/service/add','service'), {
+	name: 'service.add'
+};
+
+Router.route('/service/:_id', function () {
     var service = Services.findOne({_id: this.params._id});
     this.render('service', {data: service});
 }, {
-    name: 'services.show'
+    name: 'service.show'
 });
+
+
+
 Router.route('/js-data', 'js-data');
 Router.route('/reactive-data', 'reactive-data');
 Router.route('/layer', 'layer');
 Router.route('/servicelayer', 'servicelayer');
 Router.route('/tree', 'tree');
+Router.route('/mapnew', 'mapnew');
 
 Router.route('/maps', 'maplist');
 Router.route('/map/:_id', function () {
-    var map = Tree.findOne({_id: this.params._id});
-    this.render('map', {'selectedMapId': this.params._id});
+    var map = Maps.findOne({_id: this.params._id});
+    this.render('map');
 }, {
     name: 'map.show'
 });
+Router.route('/map/new', function () {
+    this.render('map');
+}, {
+    name: 'map.new'
+})
