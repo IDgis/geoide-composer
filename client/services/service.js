eb91fb95c72786e2ac0d734c286563b29024b0fa
LindaVels <linda.vels@idgis.nl>
2016-07-01 12:03:10 +0200

    let the GUI work with changed collection

--- client/services/service.js
+++ client/services/service.js
@@ -14,31 +14,32 @@ Template.service.helpers({
 		 }
 	},
 	serviceTypes: function () {
-		var serviceTypes = 
-			[{label: "WMS", value: "WMS"},
-		     {label: "WFS", value: "WFS"},
-		     {label: "TMS", value: "TMS"}];
-		return serviceTypes;
+		return[{label: "WMS", value: "WMS"},
+		       {label: "WFS", value: "WFS"},
+		       {label: "TMS", value: "TMS"}];
 	},
 	wmsVersions: function() {
-		var wmsVersions = 
-			[{label: "2", value: 2},
-		     {label: "3", value: 3}];
-		return wmsVersions;
+		return [{label: "1.1.1", value: "1.1.1"},
+		        {label: "1.3.0", value: "1.3.0"}];
 	},
 	wfsVersions: function() {
-		var wfsVersions = 
-			[{label: "4", value: 4},
-		     {label: "5", value: 5}];
-		return wfsVersions;
+		return [{label: "1.0.0", value: "1.0.0"},
+		        {label: "1.1.0", value: "1.1.0"},
+		        {label: "2.0.0", value: "2.0.0"}];
 	},
 	tmsVersions: function() {
-		var tmsVersions = 
-			[{label: "7", value: 7},
-		     {label: "8", value: 8}];
-		return tmsVersions;
+		return [{label: "1.0.0", value: "1.0.0"}];
 	}
 });
+
+
+
+if (this.service_type==='WFS') {
+	return ["1.0.0","1.1.0","2.0.0"];
+} 
+if (this.service_type==='TMS') {
+	return ["1.0.0"];
+}
 
 
 Template.service.events({
