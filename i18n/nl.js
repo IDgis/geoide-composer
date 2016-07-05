ed15cf33db0781000d8fec7fa9d995dd34b1a273
LindaVels <linda.vels@idgis.nl>
2016-07-05 09:34:06 +0200

    layer schema verbetert

--- i18n/nl.js
+++ i18n/nl.js
@@ -1,65 +1,122 @@
 i18n.map('nl', {
-  // GUI
-  main : {
-    title : 'Geoide Admin',
-    menu  : {
-      services: 'Services',
-      layers: 'Lagen',
-      maps: 'Kaarten',
-      i18n: 'i18n - test',
-      jsonapi: 'jsonapi - test',
-      xmlapi: 'xmlapi - test',
-    },
-    messages : 'meldingen'
-  },
-  
-  services:{
-    
-  },
-  
-  layers: {
-    
-  },
-  
-  maps: {
-    
-  },
-  
-  // Collections
-  collections: {
-    layers: {
-      name: {
-        label: 'Naam van de laag',
-      },
-      type: {
-        label: 'Type van de laag',
-      },
-    },
-    
-    services: {
-      name: {
-        label: 'Naam',
-      },
-      endpoint: {
-          label: 'Url',
-      },
-      type: {
-    	  label: 'Type',
-      },
-      version: {
-    	  label: 'Versie',
-      },
-    }, 
-  },
+	// GUI
+	main : {
+		title : 'Geoide Admin',
+		menu : {
+			services : 'Services',
+			layers : 'Lagen',
+			maps : 'Kaarten',
+			i18n : 'i18n - test',
+			jsonapi : 'jsonapi - test',
+			xmlapi : 'xmlapi - test',
+		},
+		messages : 'meldingen'
+	},
 
-  // Generiek
-  version: 'versie',
+	services : {
 
-// test i18n  
-  heading1 : 'Hoofdstuk 1',
-  heading2 : {
-    main : 'hoofdstuk 2',
-    sub : 'deel 2',
-  }
-  
+	},
+
+	layers : {
+
+	},
+
+	maps : {
+
+	},
+
+	// Collections
+	collections : {
+		layers : {
+			name : {
+				label : 'Naam',
+			},
+			type : {
+				label : 'Type',
+			},
+			service_layers : {
+				label : 'Servicelagen',
+			},
+			properties : {
+				label : 'Eigenschappen',
+				initial_visible : {
+					label : 'Initieel zichtbaar',
+				},
+				initial_query : {
+					label : 'Initiële query',
+				},
+				applayer : {
+					label : 'CRS2 laag',
+				},
+			},
+			service_layer : {
+				name : {
+					label : 'Naam',
+				},
+				service : {
+					label : 'Service',
+				},
+				nameInService : {
+					label : 'Servicelaagnaam',
+				},
+				wfs_service : {
+					label : 'WFS service',
+				},
+				featureTypes : {
+					label : 'WFS Featuretypes'
+				},
+				featureType : {
+					name : {
+						label : 'Naam',
+					},
+					service : {
+						label : 'Service',
+					},
+					nameInService : {
+						label : 'FeatureType naam'
+					},
+					searchTemplates : {
+						label : 'Zoekingangen'
+					},
+					searchTemplate : {
+						label : {
+							label : 'Label',
+						},
+						attribute_localname : {
+							label : 'Zoekveld',
+						},
+						attribute_namespace : {
+							label : 'Namespace',
+						},
+					},
+				},
+			},
+		},
+
+		services : {
+			name : {
+				label : 'Naam',
+			},
+			endpoint : {
+				label : 'Url',
+			},
+			type : {
+				label : 'Type',
+			},
+			version : {
+				label : 'Versie',
+			},
+		},
+	},
+
+	// Generiek
+	version : 'versie',
+
+	// test i18n
+	heading1 : 'Hoofdstuk 1',
+	heading2 : {
+		main : 'hoofdstuk 2',
+		sub : 'deel 2',
+	}
+
 });
--- imports/api/collections/layers.js
+++ imports/api/collections/layers.js
@@ -3,130 +3,24 @@ import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
 export const Layers = new Mongo.Collection("layers");
 
-SimpleSchema.qName = new SimpleSchema ({
-	localname: {
-		type: String,
-		label: "Naam"
-	},
-	namespace: {
-		type: String,
-		label: "Namespace",
-		optional: true
-	}
-})
-
-SimpleSchema.queryDescription = new SimpleSchema ({
-	label: {
-		type: String,
-		label: "Label"
-	},
-	attribute : {
-		type: SimpleSchema.qName,
-		label: "Attribuutveld"
-	}		
-	
-})
-
-SimpleSchema.featureType = new SimpleSchema ({
-	label: {
-		type: String,
-		label: "Label"
-	}, 
-	service: {
-		type: String,
-		label: "Service"
-	},
-	name: {
-		type: String,
-		label: "Naam"
-	},
-	queryDescriptions: {
-	    	type: [SimpleSchema.queryDescription],
-	    	label: "Zoekingangen",
-	    	optional: true
-	}   
-})
-
-SimpleSchema.serviceLayer = new SimpleSchema ({
-	label: {
-		type: String,
-		label: "Label"
-	}, 
-	service: {
-		type: String,
-		label: "Service",
-		autoform: {
-			type: 'select',
-		}		
-	},
-	name: {
-		type: String,
-		label: "Naam"
-	}, 
-	featureType: {
-		type: SimpleSchema.featureType,
-		optional: true
-	}
-});
-
-SimpleSchema.layerState = new SimpleSchema ({
-	visible: {
-		type: Boolean,
-		optional: true,
-		label: "Standaard zichtbaar"
-	},
-	query: {
-		type: String,
-		optional: true,
-	},	
-})
-
-SimpleSchema.layerProperties = new SimpleSchema ({
-	applayer: {
-		type: Boolean,
-		optional: true,
-		label: "CRS2 laag"
-	}
-})
 
 Layers.attachSchema(new SimpleSchema({
 	label: {
 		type: String,
-		label: "Label"
+		label: function(){ return i18n('collections.layers.name.label'); },
 	}, 
-	layer_type: {
+	type: {
 		type: String,
-		label: "Type",
+		label: function(){ return i18n('collections.layers.type.label'); },
 		allowedValues: ['default','cosurvey-sql'],
-		autoform: {
-		  type: 'select-radio-inline', 
-		  defaultValue: "default",
-		  options: function() {
-            return [{
-                label: "default",
-                value: "default",
-            }, {
-                label: "cosurvey-sql",
-                value: "cosurvey-sql",
-            }];
-		  }
-		}
 	}, 
 	service_layers: {
 		type: [SimpleSchema.serviceLayer],
-		label: "Servicelagen",
-		optional: true,
-		autoform: {
-			
-		}		
+		label: function(){ return i18n('collections.layers.service_layers.label'); },	
 	}, 
-	state: {
-		type: SimpleSchema.layerState,
-		label: "Toestand"
-	},
 	properties:  {
 		type: SimpleSchema.layerProperties,
-		label: "Extra Properties"
+		label: function(){ return i18n('collections.layers.properties.label'); },
 	}
 	
 }));
@@ -136,5 +30,92 @@ Layers.allow({
 	  update: function () { return true; },
 	  remove: function () { return true; }
 });
+
+
+SimpleSchema.layerProperties = new SimpleSchema ({
+	initial_visible: {
+		type: Boolean,
+		optional: true,
+		label: function(){ return i18n('collections.layers.properties.initial_visible.label'); },
+	},
+	initial_query: {
+		type: String,
+		optional: function() {
+			if (this.type==='cosurvey-sql') {
+				return false;
+			} else {
+				return true;
+			}	
+		 },
+		 label: function(){ return i18n('collections.layers.properties.initial_query.label'); },
+	},	
+	applayer: {
+		type: Boolean,
+		defaultValue: false,
+		label: function(){ return i18n('collections.layers.properties.applayer.label'); },
+	}
+})
+
+SimpleSchema.serviceLayer = new SimpleSchema ({
+	name: {
+		type: String,
+		label: function(){ return i18n('collections.layers.servicelayer.name.label'); },
+	}, 
+	service: {
+		//services_id
+		type: String,
+		label: function(){ return i18n('collections.layers.servicelayer.service.label'); },	
+	},
+	nameInService: {
+		type: String,
+		label: function(){ return i18n('collections.layers.servicelayer.nameInService.label'); },
+	}, 
+	featureType: {
+		type: SimpleSchema.featureType,
+		optional: true,
+		label: function(){ return i18n('collections.layers.servicelayer.featureTypes.label'); },
+	},
+});
+
+SimpleSchema.featureType = new SimpleSchema ({
+	name: {
+		type: String,
+		label: function(){ return i18n('collections.layers.servicelayer.featureType.name.label'); },
+	}, 
+	service: {
+		type: String,
+		label: function(){ return i18n('collections.layers.servicelayer.featureType.service.label'); },
+	},
+	nameInService: {
+		type: String,
+		label: function(){ return i18n('collections.layers.servicelayer.featureType.nameInService.label'); },
+	},
+	searchTemplatess: {
+    	type: [SimpleSchema.searchTemplate],
+    	label: function(){ return i18n('collections.layers.servicelayer.featureType.searchTemplates.label'); },
+    	optional: true,
+	},   
+})
+
+
+SimpleSchema.searchTemplate = new SimpleSchema ({
+	label: {
+		type: String,
+		label: function(){ return i18n('collections.layers.servicelayer.featureType.searchTemplate.label.label'); },
+	},
+	attribute_localname : {
+		type: String,
+		label: function(){ return i18n('collections.layers.servicelayer.featureType.searchTemplate.attribute_localname.label'); },
+	},
+	attibute_namespace: {
+		type: String,
+		label: function(){ return i18n('collections.layers.servicelayer.featureType.searchTemplate.attribute_namespace.label'); },
+		optional: true
+	}		
+})
+
+
+
+
 
 
