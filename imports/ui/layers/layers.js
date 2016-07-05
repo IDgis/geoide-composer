1174469ea5fff88a25285ffac1195c45e33a3d94
RobStek <rob.stekelenburg@idgis.nl>
2016-07-05 09:19:32 +0200

    add layers list

---
+++ imports/ui/layers/layers.js
@@ -0,0 +1,41 @@
+import { Template } from 'meteor/templating';
+import { Router } from 'meteor/iron:router';
+import { Session } from 'meteor/session';
+
+import { Layers } from '/imports/api/collections/layers.js';
+
+import './layers.html';
+
+Template.layers.helpers({
+	/**
+	 * List of layers
+	 */
+  layers: function(){
+        return Layers.find();
+    },
+    isSelectedLayer: function () {
+		return Session.equals("selectedLayerId", this._id);
+	},
+});
+
+
+Template.layers.events({
+  'click .edit-layer': function () { 
+	  Session.set("selectedLayerId", this._id);
+    console.log("edit layer " + this._id); 
+	  Router.go('layer.edit', {_id: this._id});
+  },
+  'click .insert-layer': function () {
+	  Session.set("selectedLayerId", null);
+    console.log("insert layer "); 
+	  Router.go('layer.insert');
+  },
+  'click .delete-layer': function() {
+	  //zie https://github.com/aldeed/meteor-delete-button
+	 console.log("verwijder layer " + this._id); 
+	 Layers.remove({_id:this._id})
+  },
+});
+
+
+
