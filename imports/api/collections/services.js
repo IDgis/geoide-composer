13d9e16a24adaf021b98d9ecf3ecceb4cf4ac373
LindaVels <linda.vels@idgis.nl>
2016-08-25 17:49:26 +0200

    Nieuwe regEx voor validatie endpoint

--- imports/api/collections/services.js
+++ imports/api/collections/services.js
@@ -21,10 +21,13 @@ export const ServiceSchema = new SimpleSchema({
 	endpoint: {
 		type: String,
 		label: function(){ return i18n('collections.services.endpoint.label'); },
-		regEx: SimpleSchema.RegEx.Url,
-    autoform: {
-      "title": function(){ return i18n ('tooltips.services.autoform.fields.endpoint'); },
-    },
+		regEx: /((http:|https:)\/\/[a-zA-Z0-9\.-]{2,}(:[0-9]{2,})?(\/)[a-zA-Z0-9\/\.](\?)?)/,
+		//regex url was
+		/*/^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/i,
+	    */
+	    autoform: {
+	      "title": function(){ return i18n ('tooltips.services.autoform.fields.endpoint'); },
+	    },
 	},
 	type: {
 		type: String,
