fbdc2b0197c820771dfb1c116c797869ae216707
RobStek <rob.stekelenburg@idgis.nl>
2016-10-05 15:37:50 +0200

    assemble WMS/TMS select options in server code

--- imports/startup/server/parse_capabilities.js
+++ imports/startup/server/parse_capabilities.js
@@ -229,9 +229,9 @@ Meteor.methods({
               if (mainLayer.$.queryable == '1'){
                 level = 2;
                 if (mainLayer.Name){
-                  servoptions.push({name:mainLayer.Name[0], title:mainLayer.Title[0]});
+                  servoptions.push({value:mainLayer.Name[0], label:mainLayer.Title[0]});
                 } else {
-                  servoptions.push({name:'', title:mainLayer.Title[0]});
+                  servoptions.push({value:'', label:mainLayer.Title[0], disabled:true});
                 }              }
             }
           }
@@ -248,7 +248,7 @@ Meteor.methods({
         if (!errorMsg){
           errorMsg = xmlResponse.response.statusCode;
         }
-        sortedServoptions.push({name:WMSLAYERSKEY, title:'[Error: '+errorMsg+']'});
+        sortedServoptions.push({value:WMSLAYERSKEY, label:'[Error: '+errorMsg+']', disabled:true});
       }
 //      console.log('WMS Layers found: ',sortedServoptions);
     }
@@ -270,9 +270,9 @@ Meteor.methods({
                 let titleWithPrefix = (prefix + ' ' +  subLayer.Title[0])
                 if (subLayer.Name){
 //                console.log('titleWithPrefix', level, titleWithPrefix);
-                  servoptions.push({name:subLayer.Name[0], title:titleWithPrefix});
+                  servoptions.push({value:subLayer.Name[0], label:titleWithPrefix});
                 } else {
-                  servoptions.push({name:'', title:titleWithPrefix});
+                  servoptions.push({value:'', label:titleWithPrefix, disabled:true});
                 }
               }
             }
@@ -303,7 +303,11 @@ Meteor.methods({
          * get the title from the TileMap and use this as layername and title
          */
         var layername =  parseResponse.TileMap.Title;//href.slice(first, last);
-        servoptions.push({name:layername, title:layername});
+        if (layername){
+          servoptions.push({label:layername, value:layername});
+        } else {
+          servoptions.push({label:'not found', value:'not found', disabled:true});
+        }
         TMSLAYERS.set(TMSLAYERSKEY, servoptions);
       } else {
         console.log('getTmsLayers ERROR xmlResponse:', xmlResponse);
@@ -311,7 +315,7 @@ Meteor.methods({
         if (!errorMsg){
           errorMsg = xmlResponse.response.statusCode;
         }
-        servoptions.push({name:TMSLAYERSKEY, title:'[Error: '+errorMsg+']'});
+        servoptions.push({value:TMSLAYERSKEY, label:'[Error: '+errorMsg+']', disabled:true});
       }
     }
 
@@ -507,7 +511,9 @@ Meteor.methods({
           }
           console.log('WMS getLegendGraphic capLayer url: ',result);
           if (!result){
-            // there is no legendgraphic url in the layer itself, use the general one 
+            /*
+             *  there is no legendgraphic url in the layer itself, use the general one
+             */ 
             var capRequest = capObject.Request;
 //            console.log('WMS getLegendGraphic capRequest: ',capRequest);
             if (capRequest){
@@ -567,7 +573,6 @@ Meteor.methods({
                 } else {
                   url = null;
                 }
-            
                 result = url;
               }
             }
