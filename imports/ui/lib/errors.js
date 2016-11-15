08a2b4dd4c1890834278d68ef033c07e294d9e84
RobStek <rob.stekelenburg@idgis.nl>
2016-11-15 13:38:06 +0100

    removed error.js, was not used

--- imports/ui/lib/errors.js
+++
@@ -1,9 +0,0 @@
-import { i18n } from 'meteor/anti:i18n';
-
-export const displayError = (error) => {
-  if (error) {
-    // It would be better to not alert the error here but inform the user in some
-    // more subtle way
-    alert(i18n(error.error));
-  }
-};
